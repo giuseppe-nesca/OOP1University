f8b1b4b74760659b5e99ae05e787157893f154b9
Peppe <giuseppenesca.g@gmail.com>
2017-03-25 19:32:33 +0100

    start

---
+++ src/university/University.java
@@ -0,0 +1,56 @@
+package university;
+
+public class University {
+
+	public University(String name){
+		//TODO: to be implemented
+	}
+	
+	public String getName(){
+		//TODO: to be implemented
+		return null;
+	}
+	
+	public void setRector(String first, String last){
+		//TODO: to be implemented
+	}
+	
+	public String getRector(){
+		//TODO: to be implemented
+		return null;
+	}
+	
+	public int enroll(String first, String last){
+		//TODO: to be implemented
+		return -1;
+	}
+	
+	public String student(int id){
+		//TODO: to be implemented
+		return null;
+	}
+	
+	public int activate(String title, String teacher){
+		//TODO: to be implemented
+		return -1;
+	}
+	
+	public String course(int code){
+		//TODO: to be implemented
+		return null;
+	}
+	
+	public void register(int studentID, int courseCode){
+		//TODO: to be implemented
+	}
+	
+	public String listAttendees(int courseCode){
+		//TODO: to be implemented
+		return null;
+	}
+
+	public String studyPlan(int studentID){
+		//TODO: to be implemented
+		return null;
+	}
+}
